e1d9b0826e403f36301562adcf0f370c8e1655cd
Paola Giovanna Lino Barrón <AL05054334@tecmilenio.mx>
2024-01-26 19:11:48 -0600

    En este commit estoy terminando la calculadora, corrigiendo errores y agregando la documentación de mi código. Trabajo aún sin terminar.

--- src/edu/pao/reto1/ui/CLI.java
+++ src/edu/pao/reto1/ui/CLI.java
@@ -8,55 +8,63 @@ import java.util.Scanner;
  * Esta clase sirve para que el usuario interactue con la interfaz, ingresando sus datos (peso y estatura)
  * dependiendo del cálculo que necesite realizar.
  **/
+
 public class CLI
 {
-    public static void showMenu();
+    public static void main(String[] args)
+    {
+        launchApp();
+    }
+    /**
+     * Este método mostrará en pantalla el menú, junto con las opciones de las operaciones que el usuario
+     * puede realizar.
+     **/
+    public static void showMenu()
     {
         System.out.println("Ingrese la opción para el cálculo que desea operar:");
         System.out.println("A) Cálculo de masa corporal (índice de masa corporal).");
         System.out.println("B) Cálculo de masa corporal magra.");
         System.out.println("C) Cálculo de metabolismo basal (gasto energético basal).");
         System.out.println("D) Salir.");
-
-        {
-            public static void launchApp()
-                Scanner scanner = new Scanner(System.in);
-                while (true)
-                {
-                    showMenu();
-                    String opcion = scanner.nextLine();
-
-
-                    switch (opcion)
-                    {
-                        case "A":
-                           double indiceMasaCorporal = CalculadoraMasaCorporal.calcularIndiceMasaCorporal(estatura: 173, peso: 60)
-                            String estado = clasificarIndiceMasaCorporal(indiceMasaCorporal);
-                            System.out.printf("Tu índice de masa corporal es %f");
-                            break;
-
-                        case "B":
-                            CalcularMasaCorporalMagra();
-
-                            break;
-
-                        case "C":
-                            CalcularMetabolismoBasal();
-
-                            break;
-
-                        case "D":
-                            Salir();
-
-                            break;
-
-                        default:
-                            System.out.println("Opción no válida. Por favor, seleccione A, B o C.");
-
-
-                    }
-                }
-        }
     }
 
+    public static void launchApp()
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            showMenu();
+            String opcion = scanner.nextLine();
+
+            switch (opcion.toUpperCase())
+            {
+                case "A":
+                    double indiceMasaCorporal = CalculadoraMasaCorporal.calcularIndiceDeMasaCorporal();
+                    String estado = CalculadoraMasaCorporal.interpretarIndiceMasaCorporal(indiceMasaCorporal);
+                    System.out.println("Tu índice de masa corporal es "+ indiceMasaCorporal + " y tu estado es: " + estado);
+                    break;
+
+                case "B":
+                    double masaMagra = CalculadoraMasaCorporal.calcularMasaCorporalMagra();
+                    System.out.println("Masa Corporal Magra: " + masaMagra + " kg");
+                    break;
+
+                case "C":
+                    double metabolismoBasal = CalculadoraMasaCorporal.calcularMetabolismoBasal();
+                    System.out.println("Gasto Energético Basal: " + metabolismoBasal);
+                    break;
+
+                case "D":
+                    System.out.println("Saliendo del programa.");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione A, B, C o D.");
+            }
+        }
+    }
 }
+
+
+
