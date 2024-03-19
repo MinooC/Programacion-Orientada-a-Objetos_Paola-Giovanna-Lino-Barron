3ee5475ec0815aafa6fe4edf213fcf064f978dbc
Paola Giovanna Lino Barrón <AL05054334@tecmilenio.mx>
2024-03-18 20:09:10 -0600

    Reto 7 terminado

---
+++ src/edu/pao/reto7/ui/CLI.java
@@ -0,0 +1,47 @@
+package edu.pao.reto7.ui;
+import edu.pao.reto7.process.*;
+import java.util.Scanner;
+
+/**
+ * Esta clase sirve para que el usuario interactue con la interfaz, escribiendo la opción de la
+ * operación que desee realizar.
+ **/
+
+public class CLI
+{
+    public static void ShowMenu()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenido/a");
+        System.out.println("Menú de opciones:");
+        System.out.println("1) Suma");
+        System.out.println("2) Resta");
+        System.out.println("3) Multiplicación");
+        System.out.println("4) Módulo");
+        System.out.println("5) Potencia");
+        System.out.println("Ingrese la operación que desee realizar:");
+        int opcion = scanner.nextInt();
+
+        switch (opcion)
+        {
+            case 1:
+                Suma.suma();
+                break;
+            case 2:
+                Resta.resta();
+                break;
+            case 3:Multiplicacion.multiplicacion();
+                break;
+            case 4:
+                Modulo.modulo();
+                break;
+            case 5:
+                Potencia.potencia();
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+        scanner.close();
+    }
+}
